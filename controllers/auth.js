0ff7b1699ab33994501db6ea78490dba314082a5
sarmientolm <junior.sarmiento@ext.leroymerlin.es>
2022-12-15 16:05:54 +0100

    Authentication with Google Sign In Added.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const generateJWT = require("../helpers/generar-jws");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
 
@@ -41,7 +42,52 @@ const login = async (req, res = response) => {
             msg: 'Contacte con el administrador.'
         });
     }
+}
+
+const googleSignIn = async (req, res = response) => {
+    const { id_token } = req.body;
+
+    try {
+        const { name, img, email } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ email });
+
+        if (!usuario) {
+            const data = {
+                name,
+                email,
+                password: 'PWD',
+                img,
+                google: true, 
+                role: 'USER_ROLE'
+            }
+
+            usuario = new Usuario(data);
+
+            await usuario.save();
+        }
+
+        if (!usuario.status) {
+            return res.status(401).json({
+                msg: 'Pongase en contacto con el administrador, Usuario Bloqueado!'
+            });
+        }
+        const token = await generateJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se ha podido verificar'
+        });
+    }
 
 }
 
-module.exports = login;
+module.exports = {
+    login,
+    googleSignIn
+};
